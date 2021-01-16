9adedd2e08237f6085764138cce86b8e41c0a9d1
ImLena <lenasova2002@gmail.com>
2021-01-16 19:43:18 +0300

    коммит

--- src/main/java/PointsDB.java
+++ src/main/java/PointsDB.java
@@ -3,6 +3,7 @@ import javax.inject.Named;
 import javax.transaction.*;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Named("pointDB")
--- src/main/webapp/resources/js/canvas.js
+++ src/main/webapp/resources/js/canvas.js
@@ -1,7 +1,6 @@
 let x_inp = document.getElementById("formtab:inX");
 let y_save = 0;
 let x_save = 0;
-// let x_bean = document.getElementById("formtab:x_bean");
 let r_inp = document.getElementById("formtab:inR");
 let y_inp = document.getElementById("formtab:inY");
 let html = document.getElementById("canvas");
@@ -23,33 +22,33 @@ function drawCanvas() {
     ctx.lineWidth = 2;
 
     ctx.beginPath();
-//    оси
-//    x
+// оси
+// x
     ctx.moveTo(0, ctx.canvas.height / 2);
     ctx.lineTo(ctx.canvas.width, ctx.canvas.height / 2);
 
-    // y
+// y
     ctx.moveTo(ctx.canvas.width / 2, 0);
     ctx.lineTo(ctx.canvas.width / 2, ctx.canvas.height);
 
     ctx.stroke();
 
-    // стрелки
-    //x
+// стрелки
+//x
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width-10, ctx.canvas.height/2-3)
     ctx.lineTo(ctx.canvas.width, ctx.canvas.height/2);
     ctx.lineTo(ctx.canvas.width-10, ctx.canvas.height/2+3);
     ctx.stroke();
 
-    //y
+//y
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/2-3, 10);
     ctx.lineTo(ctx.canvas.width/2, 0);
     ctx.lineTo(ctx.canvas.width/2+3, 10);
     ctx.stroke();
 
-    //    I
+// I
     ctx.beginPath();
     ctx.strokeStyle="grey";
     ctx.lineWidth = 2;
@@ -58,8 +57,7 @@ function drawCanvas() {
     ctx.rect(ctx.canvas.width/2, ctx.canvas.height/8, 3*ctx.canvas.width/8, 3*ctx.canvas.height/8);
     ctx.stroke();
 
-
-    //      II
+// II
     ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.fillStyle="lightgreen";
@@ -70,7 +68,7 @@ function drawCanvas() {
     ctx.stroke();
     ctx.fill();
 
-    //      IV
+// IV
     ctx.beginPath();
     ctx.fillStyle="#C07EDF";
     ctx.arc(ctx.canvas.width/2, ctx.canvas.width/2, (3/8)*ctx.canvas.height, 0, Math.PI/2);
@@ -80,58 +78,54 @@ function drawCanvas() {
     ctx.stroke();
     ctx.fill();
 
-
-
-    //    разделения
-    //    y
-    //R
+// разделения
+// y
+//R
     ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/2.05, ctx.canvas.height/8);
     ctx.lineTo(ctx.canvas.width/1.95, ctx.canvas.height/8);
     ctx.stroke();
-    //R/2
+//R/2
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/2.05, 2.5*ctx.canvas.height/8);
     ctx.lineTo(ctx.canvas.width/1.95, 2.5*ctx.canvas.height/8);
     ctx.stroke();
-    //-R
+//-R
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/2.05, 7*ctx.canvas.height/8);
     ctx.lineTo(ctx.canvas.width/1.95, 7*ctx.canvas.height/8);
     ctx.stroke();
-    //-R/2
+//-R/2
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/2.05, 5.5*ctx.canvas.height/8);
     ctx.lineTo(ctx.canvas.width/1.95, 5.5*ctx.canvas.height/8);
     ctx.stroke();
 
-    //    x
-    //-R
+// x
+//-R
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width/8, ctx.canvas.height/2.05);
     ctx.lineTo(ctx.canvas.width/8, ctx.canvas.height/1.95);
     ctx.stroke();
-    //-R/2
+//-R/2
     ctx.beginPath();
     ctx.moveTo(2.5*ctx.canvas.width/8, ctx.canvas.height/2.05);
     ctx.lineTo(2.5*ctx.canvas.width/8, ctx.canvas.height/1.95);
     ctx.stroke();
-    //R/2
+//R/2
     ctx.beginPath();
     ctx.moveTo(5.5*ctx.canvas.width/8, ctx.canvas.height/2.05);
     ctx.lineTo(5.5*ctx.canvas.width/8, ctx.canvas.height/1.95);
     ctx.stroke();
-    //R
+//R
     ctx.beginPath();
     ctx.moveTo(7*ctx.canvas.width/8, ctx.canvas.height/2.05);
     ctx.lineTo(7*ctx.canvas.width/8, ctx.canvas.height/1.95);
     ctx.stroke();
 
-
     rSign(r);
     loadDots();
-
 
 }
 
@@ -146,7 +140,9 @@ function rSign(r){
     ctx.fillText('-'+(r/2).toString(), ctx.canvas.width/1.85, 5.5*ctx.canvas.height/8);
 
     ctx.fillText(r.toString(), 7*ctx.canvas.width/8, ctx.canvas.height/2.15);
-    ctx.fillText(r.toString(), ctx.canvas.width/1.85, ctx.canvas.height/8);
+    ctx.fillText(r.toString(), ctx.canvas.width/1.85,
+
+        ctx.canvas.height/8);
 
     ctx.fillText((r/2).toString(), 5.5*ctx.canvas.width/8, ctx.canvas.height/2.15);
     ctx.fillText((r/2).toString(), ctx.canvas.width/1.85, 2.5*ctx.canvas.height/8);
@@ -179,15 +175,10 @@ function moveDot(){
     console.log(y);
 }
 
-
 x_inp.addEventListener("input", function (){
     x_save = x_inp.value;
     moveDot();
 })
-
-// y_inp.addEventListener("input", function (){
-//     moveDot();
-// })
 
 r_inp.addEventListener("change", function (){
     moveDot();
@@ -208,10 +199,6 @@ function drawDot(){
         y = ((ctx.canvas.height / 2 - cy) / (ctx.canvas.width / 2 - ctx.canvas.height / 8)) * r;
 
         if (validate()) {
-            // ctx.beginPath();
-            // ctx.fillStyle = "#5611BE";
-            // ctx.arc(cx, cy, ctx.canvas.height / 100, 0, Math.PI * 2);
-            // ctx.fill();
             oneDot(cx, cy);
             x_inp.value = x.toString();
             y_inp.value = y.toString();
@@ -221,7 +208,6 @@ function drawDot(){
             y = y_save;
             x_inp.value = x_save;
             x = x_save;
-
 
         } else {
             if (!((x>-3) && (x<3)))
@@ -250,9 +236,6 @@ function loadDots(){
         }
     }
 
-
-
-
     for (let i = 0; i < arr_x.length; i++){
         let cx = (arr_x[i] / r) * (7 * ctx.canvas.width / 8 - ctx.canvas.width / 2) + ctx.canvas.width / 2;
         arr_cx.push(cx);
@@ -265,10 +248,9 @@ function loadDots(){
     for (let i = 0; i < arr_res.length; i++){
         ctx.beginPath();
         if(arr_res[i] === "\n" +
-            "                            true\n" +
-            "                        ") ctx.fillStyle = "green"
+            " true\n" +
+            " ") ctx.fillStyle = "green"
         else ctx.fillStyle = "darkred"
-        // console.log(arr_res);
         ctx.arc(arr_cx[i], arr_cy[i], ctx.canvas.height/100, 0, Math.PI * 2);
         ctx.fill();
     }
@@ -283,18 +265,16 @@ function addDot() {
     console.log(y);
     console.log(r);
 
-
     console.log(check)
     arr_x.push(x);
     arr_y.push(y);
     arr_r.push(r);
     arr_res.push("\n" +
-        "                            " + check.toString() + "\n" +
-        "                        ");
+        " " + check.toString() + "\n" +
+        " ");
 
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     drawCanvas();
-
 
 }
 
